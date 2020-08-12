38b9ffcc2260162b0a48a259666879c91cc333fc
Tim Kilian <tim.kilian@edel-optics.de>
2020-08-12 10:46:35 +0200

    Remove bootstrap.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
-require('./bootstrap');
+window._ = require('lodash');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import Vue from 'vue';
 
