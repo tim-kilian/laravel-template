bc1c0a79786864ec2efd5d40e554904d03e2dbcd
Tim Kilian <tim.kilian@edel-optics.de>
2020-08-12 09:27:46 +0200

    Add user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder {
 
     public function run() {
-        // $this->call(UserSeeder::class);
+//         $this->call(UserSeeder::class);
     }
 
 }
