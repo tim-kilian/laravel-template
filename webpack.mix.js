ca5ffe08147ae99a4c14c257a651db51b6bd166d
Tim Kilian <tim.kilian@edel-optics.de>
2020-08-12 09:12:03 +0200

    Use imagemin to minify images

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,12 +4,39 @@ require('laravel-mix-purgecss');
 
 mix.js(
     'resources/js/app.js',
-    'public/js'
+    'public/js',
 ).postCss(
     'resources/css/app.css',
     'public/css',
-    require('tailwindcss')
+    require('tailwindcss'),
 );
+
+mix.copy('resources/fonts', 'public/fonts');
+
+mix.webpackConfig({
+    plugins: [
+        new (require('copy-webpack-plugin'))({
+            patterns: [
+                { from: 'resources/images', to: 'images/' },
+            ],
+        }),
+        new (require('imagemin-webpack-plugin').default)({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            pngquant: { quality: '80-90' },
+            plugins: [
+                require('imagemin-gifsicle')({ interlaced: false }),
+                require('imagemin-mozjpeg')({ arithmetic: false }),
+                require('imagemin-pngquant')({ floyd: 0.5, speed: 2 }),
+                require('imagemin-svgo')({
+                    plugins: [
+                        { removeTitle: true },
+                        { convertPathData: false },
+                    ],
+                }),
+            ],
+        }),
+    ],
+});
 
 if (mix.inProduction()) {
     mix.version().purgeCss();
