c89f4c3209ccfc5331ef29decbad83d6c7103281
Tim Kilian <tim.kilian@edel-optics.de>
2020-08-12 10:05:31 +0200

    Add 404 error handler

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container mx-auto h-full">
+    <div class="h-full flex items-center mx-5">
+      <img
+          src="{{ asset('/images/illustrations/undraw_page_not_found.svg') }}"
+          alt="404"
+      >
+    </div>
+  </div>
+@endsection
