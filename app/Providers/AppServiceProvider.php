58a375b2f505e023f96e6f3731eb88da33c942a4
Tim Kilian <tim.kilian@edel-optics.de>
2020-08-12 10:04:54 +0200

    Add ide helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,15 @@
 <?php namespace App\Providers;
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 
 class AppServiceProvider extends ServiceProvider {
 
     public function register() {
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
         //
     }
 
